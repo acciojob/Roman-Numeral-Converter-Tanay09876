e06e3d5c53b1c8b942b62c3e16166d1c99dbb350
Tanay09876 <152384332+Tanay09876@users.noreply.github.com>
2025-06-23 20:30:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
-function convertToRoman(num) {
-  if (num <= 0 || num > 100000) {
-    return "Invalid input";
-  }
+var romanToInt = function(s) {
+    let res = 0;
+    const roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
-  const romanNumerals = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
-
-  let result = "";
-
-  for (let [symbol, value] of romanNumerals) {
-    while (num >= value) {
-      result += symbol;
-      num -= value;
+    for (let i = 0; i < s.length - 1; i++) {
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            res -= roman[s[i]];
+        } else {
+            res += roman[s[i]];
+        }
     }
-  }
 
-  return result;
-}
-
-
-console.log(convertToRoman(14)); 
-console.log(convertToRoman(798));   
+    return res + roman[s[s.length - 1]];    
+};
